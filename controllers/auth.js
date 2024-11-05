67ba1638d16681d992db52df314aa8fe3cbc5dee
Emrah Akçal <emrah.akcal@impektra.com>
2024-11-05 17:19:43 +0300

    added login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 const get_register = async (req, res) => {
     try {
@@ -24,7 +25,13 @@ const post_register = async (req, res) => {
                 error: 'This email is already registered.'
             });
         }
-        await User.create(data);
+        const newUser = {
+            firstName: data.firstName,
+            lastName: data.lastName,
+            email: data.email,
+            password: await bcrypt.hash(data.password, 10)
+        }
+        await User.create(newUser);
         return res.redirect('/account/login?success=true');
     } catch (error) {
         console.log(error);
@@ -43,9 +50,38 @@ const get_login = async (req, res) => {
     }
 }
 
+const post_login = async (req, res) => {
+    try {
+        const data = req.body;
+        const user = await User.findOne({
+            where: {
+                email: data.email
+            }
+        });
+        if (!user) {
+            return res.render('account/login', {
+                title: 'Login',
+                error: 'Invalid email or password.'
+            });
+        }
+        const isPasswordMatch = await bcrypt.compare(data.password, user.password);
+        if (!isPasswordMatch) {
+            return res.render('account/login', {
+                title: 'Login',
+                error: 'Invalid email or password.'
+            });
+        }
+        return res.redirect('/');
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 
 module.exports = {
     get_register,
     post_register,
-    get_login
+    get_login,
+    post_login
 }
